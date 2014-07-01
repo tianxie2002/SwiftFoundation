a797fde494504bbf24c0f07c66a3f1343fa59b91
baxiang <baxiang1989@163.com>
2014-07-02 00:51:04 +0800

    add 增加知乎数据

---
+++ SwiftFoundation/ZhiHuViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ZhiHuViewController.swift
+//  SwiftFoundation
+//
+//  Created by baxiang on 14-7-2.
+//  Copyright (c) 2014年 baxiang. All rights reserved.
+//
+
+import UIKit
+
+class ZhiHuViewController: BaseViewController {
+
+    @IBOutlet var ZhiHuTable : UITableView = nil
+    init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        // Custom initialization
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // #pragma mark - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
